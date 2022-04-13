cdc232f1411a160859d49177517ce912b5ff1687
Wil Rohlhill <willem.rohlhill@rutgers.edu>
2022-04-13 16:12:37 -0400

    Run multiple files
    Add a .gitignore file
    Allow shen extraction for a full BIDS dataset

--- extraction/shen_extraction.py
+++ extraction/shen_extraction.py
@@ -1,25 +1,10 @@
 import os
-from nilearn import datasets
-# from nideconv.utils import roi
 from utils import roi
 import pandas as pd
-from nilearn import plotting
-from nilearn.input_data import NiftiLabelsMasker, NiftiMapsMasker
 import shen_atlas as sa
 import re
 
-base_dir = 'C:\\Users\\wr178\\PycharmProjects\\ShenExtraction'
-
-# Locate the data of the first subject
-#func = 'fmriprep\\sub-108768\\func\\sub-108768_task-gonogo_run-2_space-MNI152NLin2009cAsym_res-2_desc-preproc_bold.nii.gz'
-#func = 'fmriprep/sub-108768/func/sub-108768_task-gonogo_run-1_space-MNI152NLin6Asym_desc-smoothAROMAnonaggr_bold.nii.gz'
-#func = 'fmriprep/sub-108768/func/sub-108768_task-gonogo_run-2_space-MNI152NLin6Asym_desc-smoothAROMAnonaggr_bold.nii.gz'
-#func = 'fmriprep/sub-212291/func/sub-212291_task-gonogo_run-1_space-MNI152NLin2009cAsym_res-2_desc-preproc_bold.nii.gz'
-#func = 'fmriprep/sub-212291/func/sub-212291_task-mid_run-2_space-MNI152NLin2009cAsym_res-2_desc-preproc_bold.nii.gz'
-#func = 'fmriprep/sub-212291/func/sub-212291_task-mid_run-2_space-MNI152NLin6Asym_desc-smoothAROMAnonaggr_bold.nii.gz'
-#func = 'fmriprep/sub-212291/func/sub-212291_task-gonogo_run-1_space-MNI152NLin6Asym_desc-smoothAROMAnonaggr_bold.nii.gz'
-func = 'fmriprep/sub-212291/func/sub-212291_task-cap_run-2_space-MNI152NLin6Asym_desc-smoothAROMAnonaggr_bold.nii.gz'
-
+"""
 # Get meta data for this file
 p = re.compile(r'sub-(?P<sub>\d+)_task-(?P<task>\w+)_run-(?P<run>\d+)_space-\w+(_res-(?P<res>\d+))*_desc-(?P<desc>\w+)_bold')
 m = p.search(func)
@@ -34,47 +19,60 @@ else:
 
 if 'AROMA' in desc:
     desc = 'AROMA'
-
-# ... and confounds extracted by fmriprep
-#confounds_fn = 'fmriprep\\sub-108768\\func\\sub-108768_task-gonogo_run-1_desc-confounds_timeseries.tsv'
-confounds_fn = 'fmriprep/sub-%s/func/sub-%s_task-%s_run-%s_desc-confounds_timeseries.tsv' % (sub,sub,task,run)
-#confounds_fn = 'fmriprep/sub-212291/func/sub-212291_task-mid_run-2_desc-confounds_timeseries.tsv'
-
-func = os.path.join(base_dir, func)
-confounds_fn = os.path.join(base_dir, confounds_fn)
-
-# We need to load the confounds and fill nas
-
-confounds = pd.read_table(confounds_fn).fillna(method='bfill')
+"""
+# Alright so we're gonna do things a little bit differently this time
+# This is basically an exploded view of roi.get_fmriprep_timeseries
+# TODO: Pass the fmriprep path in as an arg
+fmriprep_path = 'C:\\Users\\wr178\\Documents\\ZALD_TTS\\BIDS\\derivatives\\'
+source_path = 'C:\\Users\\wr178\\Documents\\ZALD_TTS\\BIDS\\'
+out_dir = 'C:\\Users\\wr178\\Documents\\SRC\\ShenExtraction\\timeSeries'
+if not os.path.isdir(out_dir):
+    os.mkdir(out_dir)
 
 # We only want to include a subset of confounds
 confounds_to_include = ['framewise_displacement', 'a_comp_cor_00',
                         'a_comp_cor_01', 'a_comp_cor_02', 'a_comp_cor_03',
                         'a_comp_cor_04', 'a_comp_cor_05',
                         'rot_x', 'rot_y', 'rot_z']
-confounds = confounds[confounds_to_include]
 
+for func, confounds, meta in roi.get_func_and_confounds(fmriprep_path,
+                                                         source_path):
+    # For now, this will only extract from the MNI152 space preproc files
+    print("Extracting signal from {}...".format(func.filename))
+    confounds = pd.read_table(confounds.path).fillna(method='bfill')
+    meta = func.entities
 
-# Use the Shen 2013 atlas
-atlas_shen = sa.fetch_atlas_shen(resolution_mm=res)
-#plotting.plot_prob_atlas(atlas_shen.maps)
-#plotting.plot_roi(atlas_shen.maps)
+    if meta is None:
+        t_r = 2
+    else:
+        t_r = meta["RepetitionTime"]
 
-#masker = NiftiLabelsMasker(labels_img=atlas_shen.maps, standardize=True)
-#time_series = masker.fit_transform(func, confounds=confounds_fn)
+    # Load shen atlas based on resolution
+    if "res" in func.get_entities():
+        res = int(func.entities["res"])
+    else:
+        res = 2
 
-ts = roi.extract_timecourse_from_nii(atlas_shen,
-                                     func,
-                                     confounds=confounds.values,
-                                     t_r=2,
-                                     high_pass=1./128,
-                                     )
+    # Use the Shen 2013 atlas
+    atlas_shen = sa.fetch_atlas_shen(resolution_mm=res)
 
+    ts = roi.extract_timecourse_from_nii(atlas_shen,
+                                         func.path,
+                                         confounds=confounds[confounds_to_include].values,
+                                         t_r=t_r,
+                                         high_pass=1./128,
+                                         )
+    print("Saving data...")
+    sv_dir = os.path.join(out_dir, 'sub-%s' % meta["subject"])
+    if not os.path.isdir(sv_dir):
+        os.mkdir(sv_dir)
 
-print(ts)
+    svname = 'sub-%s_task-%s_run-%d_desc-%s_bold' % (meta["subject"],
+                                                     meta["task"],
+                                                     meta["run"],
+                                                     meta["desc"])
 
-svname = 'sub-%s_task-%s_run-%s_desc-%s_bold' % (sub,task,run,desc)
+    ts.to_csv(os.path.join(sv_dir, '%s.csv' % svname))
+    # ts.to_pickle(os.path.join(base_dir, 'shen_data', '%s.pkl' % svname))
 
-ts.to_csv(os.path.join(base_dir,'shen_data','%s.csv' % svname))
-ts.to_pickle(os.path.join(base_dir,'shen_data','%s.pkl' % svname))
-#print("DONE!")
+    print("DONE!")
